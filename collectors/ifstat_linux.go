525baf371770dac48a2fda0089eaf7ac13d8bcdb
Peter Grace <pete.grace@gmail.com>
2014-09-19 14:03:13 -0600

    Remove error check on readLine since there's not much the end user can do about it anyway and for people who use Xen, it will produce an error every 15 seconds.

--- collectors/ifstat_linux.go
+++ collectors/ifstat_linux.go
@@ -7,7 +7,6 @@ import (
 	"github.com/StackExchange/scollector/metadata"
 	"github.com/StackExchange/scollector/opentsdb"
 	"github.com/StackExchange/scollector/util"
-	"github.com/StackExchange/slog"
 )
 
 func init() {
@@ -78,14 +77,11 @@ func c_ifstat_linux() (opentsdb.MultiDataPoint, error) {
 		tags := opentsdb.TagSet{"iface": intf}
 
 		// Detect speed of the interface in question
-		errspeed := readLine("/sys/class/net/"+intf+"/speed", func(speed string) error {
+		readLine("/sys/class/net/"+intf+"/speed", func(speed string) error {
 			Add(&md, "linux.net.ifspeed", speed, tags, metadata.Gauge, metadata.Megabit, "")
 			Add(&md, "os.net.ifspeed", speed, tags, metadata.Gauge, metadata.Megabit, "")
 			return nil
 		})
-		if errspeed != nil {
-			slog.Info("Error reading interface speed: " + errspeed.Error())
-		}
 		for i, v := range stats {
 			if strings.HasPrefix(intf, "bond") {
 				Add(&md, "linux.net.bond."+strings.Replace(FIELDS_NET[i], ".", "_", -1), v, opentsdb.TagSet{
